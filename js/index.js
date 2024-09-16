3667539e292ff93edbb9b86b50eb024f7cfd2e75
Nimesh-Kavinda <nimeshkavindakarunasinghe@gmail.com>
2024-09-16 13:50:46 +0530

    Start to make UI

---
+++ js/index.js
@@ -0,0 +1,44 @@
+
+let darkMode = JSON.parse(localStorage.getItem('darkMode')) || false;
+
+// darkMode function
+const activeDarkMode = () => {
+  changeDarkModeIcon();
+
+  darkMode = !darkMode;
+  JSON.stringify(localStorage.setItem('darkMode', darkMode));
+
+  darkModeToggle();
+};
+
+function darkModeToggle() {
+  if (darkMode) {
+    document.querySelector('html').setAttribute('data-bs-theme', 'dark');
+    document.querySelector('.startpage-div1').classList.add('bg-dark');
+    document.querySelector('.startpage-div2').classList.add('bg-dark');
+  } else if (!darkMode) {
+    document.querySelector('html').setAttribute('data-bs-theme', 'light');
+    document.querySelector('.startpage-div1').classList.remove('bg-dark');
+    document.querySelector('.startpage-div2').classList.remove('bg-dark');
+ }
+}
+
+function changeDarkModeIcon() {
+  const switchMode = document.getElementById('switch-mode');
+  const iTag = switchMode.querySelectorAll('i');
+
+
+  iTag.forEach((element) => {
+    element.classList.toggle('d-none');
+  });
+}
+
+if (darkMode != false) {
+  darkModeToggle();
+  changeDarkModeIcon();
+}
+
+const toggleButton = document.getElementById('div1');
+toggleButton.addEventListener('click', () => {
+  document.body.classList.toggle('dark-mode');
+});
