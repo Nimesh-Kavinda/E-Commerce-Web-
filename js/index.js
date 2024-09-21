0d11e9564d774fea11e334c9c473d9d2404fd4e8
Nimesh-Kavinda <nimeshkavindakarunasinghe@gmail.com>
2024-09-21 14:37:19 +0530

    Add a Login Form and Registration form

--- js/index.js
+++ js/index.js
@@ -1,3 +1,5 @@
+// Scroll navigation icon
+
 const scrollBtn = document.getElementById('scrollBtn');
 const scrollIcon = document.getElementById('scrollIcon');
 
@@ -48,3 +50,32 @@ scrollBtn.addEventListener('click', function() {
 
 
 window.addEventListener('scroll', updateIcon);
+
+// login form popup-function
+
+const loginbtns = document.querySelectorAll(".b_login");
+const loginform = document.querySelector(".login_from");
+const loginfirst_btns =document.querySelectorAll(".b_loginfirts");
+const registerform = document.querySelector(".register_form");
+
+loginbtns.forEach(function(loginbtn) {
+  loginbtn.addEventListener('click', function() {
+     loginform.classList.remove("d-none");
+     registerform.classList.add("d-none");
+  })});
+
+loginfirst_btns.forEach(function(loginfirst_btn) {
+  loginfirst_btn.addEventListener('click', function() {
+    loginform.classList.remove("d-none");
+  })});
+
+  // registrion form popup-function
+
+  const registerbtns = document.querySelectorAll(".b_register");
+
+  registerbtns.forEach(function(registerbtn) {
+    registerbtn.addEventListener('click', function() {
+       registerform.classList.remove("d-none");
+       loginform.classList.add("d-none");
+
+    })});
