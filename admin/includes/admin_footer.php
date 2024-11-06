fa4b1fc34d40df077afd86777fd7dc596e801dec
Nimesh-Kavinda <nimeshkavindakarunasinghe@gmail.com>
2024-11-06 13:54:31 +0530

    Edit admon footer page

--- admin/includes/admin_footer.php
+++ admin/includes/admin_footer.php
@@ -3,7 +3,7 @@
   <footer class="bg-dark text-center text-white py-2 mt-auto">
  
   <div class="text-center p-3" style="background-color: rgba(0, 0, 0, 0.2);">
-    © 2020 Copyright:
+    © 2024 Copyright:
     <a class="text-white" href="#">Web Desing Project (Second Year)</a>
   </div>
 
